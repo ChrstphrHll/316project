135dab49ecebad50514f1c8ea7a88c0e21187c36
Adam Kosinski <amk119@duke.edu>
2022-10-19 00:08:27 -0400

    Reading in test users, games, and likesgame successfully

--- db/load.sql
+++ db/load.sql
@@ -1,16 +1,18 @@
-\COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
+CREATE SEQUENCE public.users_id_seq;
+CREATE SEQUENCE public.games_id_seq;
+
+\COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV;
 -- since id is auto-generated; we need the next command to adjust the counter
 -- for auto-generation so next INSERT will not clash with ids loaded above:
 SELECT pg_catalog.setval('public.users_id_seq',
-                         (SELECT MAX(id)+1 FROM Users),
+                         (SELECT MAX(uid)+1 FROM Users),
                          false);
 
-\COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.products_id_seq',
-                         (SELECT MAX(id)+1 FROM Products),
+
+\COPY Games FROM 'Games.csv' WITH DELIMITER ',' NULL '' CSV;
+SELECT pg_catalog.setval('public.games_id_seq',
+                         (SELECT MAX(gid)+1 FROM Games),
                          false);
 
-\COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.purchases_id_seq',
-                         (SELECT MAX(id)+1 FROM Purchases),
-                         false);
+
+\COPY LikesGame FROM 'LikesGame.csv' WITH DELIMITER ',' NULL '' CSV;
