bbabdd3c91b21a8d2c167b4b98f46b7d1f93743c
Aryan Mathur <am943@duke.edu>
2022-10-19 21:27:33 -0400

    final changes to reviews

--- app/reviews.py
+++ app/reviews.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, url_for, redirect
 from flask_login import current_user
 
 from .models.review import Review
@@ -7,7 +7,15 @@ from flask import Blueprint
 bp = Blueprint('reviews', __name__)
 
 
-@bp.route('/reviews', methods=['GET'])
-def feedback():
-    reviews = Review.get_top_5(int(request.args.get("uid")))
+@bp.route('/reviews', methods=['GET','POST'])
+def getReview():
+    if(request.method=='POST'):
+        user = request.form["nm"]
+        return redirect(url_for("reviews.showReview",usr=user))
+    else: 
+        return render_template('reviewsIn.html')
+
+@bp.route('/reviews/<usr>', methods=['GET', 'POST'])
+def showReview(usr):
+    reviews = Review.get_top_5(int(usr))
     return render_template('reviews.html', review_history=reviews)
