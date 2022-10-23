e03b67206f9c42af84301ac1444f8f021ef192bf
MollyB-creator <molly.borowiak@duke.edu>
2022-10-23 17:54:32 -0400

    added method to get all games with a given mechanic

--- app/models/mechanic.py
+++ app/models/mechanic.py
@@ -4,8 +4,19 @@ class Mechanic:
         self.description = description
 
     def get(name):
-        rows = app.db.execute(
-            "SELECT "
-        )
+        rows = app.db.execute('''
+            SELECT mech_name, description
+            FROM Mechanics 
+            WHERE mech_name = :name''', 
+            name=name)
 
-        if rows is not None else None
+        return Product(*(rows[0])) if rows is not None else None
+
+    def get_all(name):
+        rows = app.db.execute('''
+            SELECT mech_name, gid
+            FROM Implements 
+            WHERE mech_name = :name''', 
+            name=name)
+
+        return Product(*row) for row in rows
