3beeb0fc7b981f7fc1f0b0e5468568c36d94b69f
MollyB-creator <57322435+MollyB-creator@users.noreply.github.com>
2022-10-24 18:23:17 -0400

    updated methods for recommendation.py; returns liked game used for recs

--- app/models/recommendation.py
+++ app/models/recommendation.py
@@ -4,6 +4,33 @@ from .game import Game
 
 class Recommendation:
     @staticmethod
+    def get_base(uid):
+        rows = app.db.execute('''
+SELECT g.gid, g.name, g.description, g.image_url, g.complexity, g.length, g.min_players, g.max_players
+FROM Games as g, LikesGame as L
+WHERE L.uid = :uid AND g.gid = L.gid
+''',
+                              uid=uid)
+        return Game(*(rows[0])) if rows is not None else None
+
+    @staticmethod
+    def get(uid, gid):
+        rows = app.db.execute('''
+    WITH liked_mech AS (SELECT I.mech_name FROM LikesGame as L, Implements as I
+    WHERE L.gid = :gid AND L.uid = :uid AND I.gid = L.gid)
+    (SELECT DISTINCT g.gid, g.name, g.description, g.image_url, g.complexity, g.length, g.min_players, g.max_players
+    FROM Games as g, liked_mech as M, Implements as Imp
+    WHERE g.gid = Imp.gid AND Imp.mech_name = M.mech_name)
+    EXCEPT
+    (SELECT DISTINCT g.gid, g.name, g.description, g.image_url, g.complexity, g.length, g.min_players, g.max_players
+    FROM Games as g, LikesGame as L
+    WHERE L.gid = :gid AND L.uid = :uid AND g.gid = L.gid)
+    ''',
+                                uid=uid, gid=gid)
+        return [Game(*row) for row in rows]
+
+    """
+    @staticmethod
     def get(uid):
         rows = app.db.execute('''
     WITH liked AS (SELECT * FROM LikesGame WHERE uid = :uid),
@@ -15,5 +42,6 @@ class Recommendation:
     ''',
                                 uid=uid)
         return [Game(*row) for row in rows]
+    """
 
     
