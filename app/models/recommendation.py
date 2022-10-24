e1376ba7443e843de59a0a61f1ea83bf1f7b0466
Adam Kosinski <amk119@duke.edu>
2022-10-23 23:57:11 -0400

    Molly's stuff all works

--- app/models/recommendation.py
+++ app/models/recommendation.py
@@ -9,7 +9,7 @@ class Recommendation:
     WITH liked AS (SELECT * FROM LikesGame WHERE uid = :uid),
     liked_mech AS (SELECT M.mech_name FROM liked as L, Mechanics as M, Implements as I
     WHERE I.gid = L.gid AND I.mech_name = M.mech_name)
-    SELECT g.gid, g.name, g.description, g.image_url, g.complexity, g.length, g.min_players, g.max_players
+    SELECT DISTINCT g.gid, g.name, g.description, g.image_url, g.complexity, g.length, g.min_players, g.max_players
     FROM Games as g, liked_mech as M, Implements as Imp
     WHERE g.gid = Imp.gid AND Imp.mech_name = M.mech_name
     ''',
