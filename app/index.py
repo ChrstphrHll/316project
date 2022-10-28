04fa1bb013b7ad199b04aa7a50e8e47f858983c3
lovellk <keganlovell@gmail.com>
2022-10-28 02:31:53 -0400

    Cleaning up

--- app/index.py
+++ app/index.py
@@ -10,12 +10,5 @@ bp = Blueprint('index', __name__)
 
 @bp.route('/')
 def index():
-    # if current_user.is_authenticated:
-    #     purchases = Purchase.get_all_by_uid_since(
-    #         current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
-    # else:
-    #     purchases = None
-    # # render the page by adding information to the index.html file
-
     return render_template('index.html')
 
