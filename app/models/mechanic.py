9c759c9a7666107abdeb7276dca44c360c3459cf
MollyB-creator <molly.borowiak@duke.edu>
2022-11-16 21:39:32 -0500

    fxn for collecting mech counts from a set of games

--- app/models/mechanic.py
+++ app/models/mechanic.py
@@ -16,19 +16,31 @@ class Mechanic:
             ''', 
             gid=gid)
 
+        for row in rows:
+            print(row)
+
         return [Mechanic(*row) for row in rows]
 
     #given a set of games, return all mechanics used, sorted by popularity
     @staticmethod
     def get_games_mechs(games):
-        rows = app.db.execute('''
-            SELECT M.*
-            FROM Mechanics as M, Implements as I
-            WHERE M.mech_name = I.mech_name AND I.gid IN :games
-            ''', 
-            games=games)
+        mech_counts = {}
 
-        return [Mechanic(*row) for row in rows]
+        for gid in games:            
+            rows = app.db.execute('''
+                SELECT M.*
+                FROM Mechanics as M, Implements as I
+                WHERE M.mech_name = I.mech_name AND I.gid =:gid
+                ''', 
+                gid=gid)
+
+            for row in rows:
+                mech = Mechanic(*row)
+                if mech_counts[mech.mech_name] is NULL:
+                    mech_counts[mech.mech_name] = 0
+                mech_counts[mech.mech_name] = mech_counts[mech.mech_name] + 1
+
+        return mech_counts
 
     @staticmethod
     def get_all():
